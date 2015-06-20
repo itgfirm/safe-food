6c518a83070dd40cc1ebbbe20d6c56643555821e
Phil Armour <phillip.armour@itgfirm.com>
2015-06-20 16:38:08 -0400

    updated the grunt file to do all tests in a single grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,36 @@ module.exports = function(grunt){
 
 	require('load-grunt-tasks')(grunt);
 
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+
 	grunt.initConfig({
-		jshint: {
+		pkg: grunt.file.readJSON('package.json'),
+    jshint: {
 			all: paths.js
 		},
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+          timeout: 10000
+        },
+        src: ['server/test/**/*.js']
+      }
+    },
 		karma: {
 			unit: {
 				configFile: 'client/test/spec/karma.config.js'
 			}
 		}
 	});
+
 	grunt.registerTask('default', ['jshint', 'karma']);
+  grunt.registerTask('test', ['jshint', 'karma', 'mochaTest']);
+
 };
