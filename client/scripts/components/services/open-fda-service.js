2c5ae5153c44672485d046d784517562dfe55520
Phil Armour <phillip.armour@itgfirm.com>
2015-06-24 03:05:55 -0400

    created a universal search form and added some input cleaning functions for search in general

--- client/scripts/components/services/open-fda-service.js
+++ client/scripts/components/services/open-fda-service.js
@@ -2,9 +2,26 @@ define([ 'angular', 'app',
 	'components/services/location-service' ],
 	function(angular, app) {
 
+		
+
 		app.service('OpenFDAService', function($http, $q, LocationService, $filter) {
+			//TODO: These should come from the app's config file:
 			var service = { meta: null },
-				baseUrl = 'https://api.fda.gov/food/enforcement.json';
+				baseUrl = 'https://api.fda.gov/food/enforcement.json',
+				apiKey = 'RQklIryrO1GobRvtpSV6W3gE6z3IXIinmqxiIiuB';
+
+
+			var state_hash;
+			//not using a promise here, so it's just assumed this will load prior to it's first use.
+			$http.get('./states_hash.json')
+			    .success(function(data) {
+			        state_hash = data;
+			        console.log('States JSON Loaded');
+			    })
+			    .error(function() {
+			        console.error('could not find state_hash.json');
+			    });
+
 
 			service.getMeta = function() {
 				var defer = $q.defer();
@@ -68,32 +85,92 @@ define([ 'angular', 'app',
 				}
 
 				var year = dateString.substr(0, 4),
-					date = dateString.substr(4, 2)
-					month = dateString.substr(6, 2);
+					month = dateString.substr(4, 2)
+					date = dateString.substr(6, 2);
 
-				return month + '/' + date + '/' + year;
+				return  month + '/' + date + '/' + year;
+			};
+
+			/**
+			 * This function is required because the OpenFDA API queries throw errors 
+			 * when they get certain characters 
+			 * and they do NOT support URL encoding of said characters
+			 * @param  {string} paramVal the raw input from the form
+			 * @return {string}          the "cleaned" input - swapping spaces for all non-alpha-numeric characters
+			 */
+			function stripIllegalchars(paramVal) {
+
+				//replacing with spaces so that it can be tokenized as words later.
+				return paramVal.replace(/[^a-zA-Z0-9 ]/g, ' ');
+
+				//rather than whitelist we can try a blacklist:
+				// return paramVal.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ' ');
+			};
+
+
+			/**
+			 * [createStateMap description]
+			 * @param  {[type]} paramVal [description]
+			 * @return {[type]}          [description]
+			 */
+			function createStateMappings(paramVal) {
+				var terms =  paramVal.split(' ');
+				var newTerms = '';
+
+				angular.forEach(terms, function(result) {
+					//ok to use "in" because we know that state_hash is a nice, clean obeject
+					if(result.toUpperCase() in state_hash) { //found a state term
+						//add the alternative for the state indicates as well as the "nationwide" term
+						var result = '('+result+'+'+state_hash[result.toUpperCase()]+'+nationwide)';
+					}
+					//add each term back into the return value
+					newTerms += newTerms ? ' '+result : result;
+				});
+				return newTerms;
+			};
+
+
+
+			/**
+			 * Joins multiple terms (seperated by whitespace) via the OpenFDA "AND" 
+			 * syntax (https://open.fda.gov/api/reference/#query-syntax)
+			 * @param  {string} paramVal String with terms seperated by whitespace
+			 * @return {string}          String with terms seperated by "+AND+"
+			 */
+			function createAndTerms(paramVal) {
+				return paramVal.split(' ').join('+AND+');
 			};
 
 			function createSearchString(params){
 				var searchString = '',
 					recall_StartDate = null,
 					recall_EndDate = null;
-				for(key in params) {
-					if(params.hasOwnProperty(key)) {
-						if(key === 'recallStartDate'){
-							recall_StartDate = params[key];
-						}else if(key === 'recallEndDate'){
-							recall_EndDate = params[key];
-						}else{
-							searchString += searchString ? '+AND+' : '';				
-							searchString += key + ':"' + params[key] + '"';
-						}							
+
+				if (params && params.generalSearch){ //Google-style search
+					searchString += createAndTerms(createStateMappings(stripIllegalchars(params.generalSearch)));
+				} else {
+
+					for(key in params) {
+						if(params.hasOwnProperty(key)) {
+							if(key === 'recallStartDate'){
+								recall_StartDate = params[key];
+							}else if(key === 'recallEndDate'){
+								recall_EndDate = params[key];
+							}else{
+								searchString += searchString ? '+AND+' : '';				
+								searchString += key + ':"' + params[key] + '"';
+							}							
+						}
 					}
+					if(recall_StartDate || recall_EndDate){
+						searchString += searchString ? '+AND+' + generateDateQueryString(recall_StartDate, recall_EndDate) :
+											generateDateQueryString(recall_StartDate, recall_EndDate);
+					}		
+
 				}
-				if(recall_StartDate || recall_EndDate){
-					searchString += searchString ? '+AND+' + generateDateQueryString(recall_StartDate, recall_EndDate) :
-										generateDateQueryString(recall_StartDate, recall_EndDate);
-				}				
+
+
+				console.log('search string is:'+searchString);
 				return searchString;
 			};
 
@@ -120,10 +197,12 @@ define([ 'angular', 'app',
 				var url = '';
 				for(key in params){
 					if(params.hasOwnProperty(key)){
-						url += ( url ? '&' : '?' ) + key + '=' + params[key];
+						//url += ( url ? '&' : '?' ) + key + '=' + params[key];
+						//simplified now that we always have the api_key before these params
+						url += '&' + key + '=' + params[key];
 					}
 				}
-				return baseURL + url;
+				return baseURL + '?api_key=' + apiKey + url;
 			}
 
 			return service;
