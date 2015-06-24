b0e70de1a0fea1101c72905b0ae727a8fb381e59
hnpatel <hpatel9667@gmail.com>
2015-06-24 00:07:52 -0400

    Replaced 'Product Type' with 'Product Description'. Modified search query to accommodate date ranges.

--- client/scripts/components/services/open-fda-service.js
+++ client/scripts/components/services/open-fda-service.js
@@ -2,7 +2,7 @@ define([ 'angular', 'app',
 	'components/services/location-service' ],
 	function(angular, app) {
 
-		app.service('OpenFDAService', function($http, $q, LocationService) {
+		app.service('OpenFDAService', function($http, $q, LocationService, $filter) {
 			var service = { meta: null },
 				baseUrl = 'https://api.fda.gov/food/enforcement.json';
 
@@ -26,7 +26,7 @@ define([ 'angular', 'app',
 
 				params.limit = params.limit || 25;
 
-				$http.get(baseUrl, { params: params })
+				$http.get(createURL(baseUrl, params))
 					.success(function(data) {
 
 						angular.forEach(data.results, function(result) {
@@ -75,16 +75,56 @@ define([ 'angular', 'app',
 			};
 
 			function createSearchString(params){
-				var searchString = '';
-
+				var searchString = '',
+					recall_StartDate = null,
+					recall_EndDate = null;
 				for(key in params) {
 					if(params.hasOwnProperty(key)) {
-						searchString += key + ':"' + params[key] + '"';
+						if(key === 'recallStartDate'){
+							recall_StartDate = params[key];
+						}else if(key === 'recallEndDate'){
+							recall_EndDate = params[key];
+						}else{
+							searchString += searchString ? '+AND+' : '';				
+							searchString += key + ':"' + params[key] + '"';
+						}							
 					}
 				}
-
+				if(recall_StartDate || recall_EndDate){
+					searchString += searchString ? '+AND+' + generateDateQueryString(recall_StartDate, recall_EndDate) :
+										generateDateQueryString(recall_StartDate, recall_EndDate);
+				}				
 				return searchString;
 			};
+
+			function generateDateQueryString(startDate, endDate) {
+				var dateQueryString = '';
+				if(startDate){
+					if(endDate){						
+						dateQueryString += 'report_date:[' + $filter('date')(new Date(startDate), "yyyyMMdd")
+										+ '+TO+' + $filter('date')(new Date(endDate), "yyyyMMdd") + ']';  
+					}else{
+						dateQueryString += 'report_date:' + $filter('date')(new Date(startDate), "yyyyMMdd");
+					}
+				}else if(endDate){
+					dateQueryString += 'report_date:' + $filter('date')(new Date(endDate), "yyyyMMdd");
+				}
+				return dateQueryString;
+			}
+
+			/*
+			 *	Needs to pass URL as a whole string otherwise AngularJS will encode '+' sign
+			 *  which is not valid url for API endpoint
+			 */
+			function createURL(baseURL, params){
+				var url = '';
+				for(key in params){
+					if(params.hasOwnProperty(key)){
+						url += ( url ? '&' : '?' ) + key + '=' + params[key];
+					}
+				}
+				return baseURL + url;
+			}
 
 			return service;
 		});
