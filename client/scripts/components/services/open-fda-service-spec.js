f5327fb2b0278dac959390ac786fbfbb752204ec
Phil Armour <phillip.armour@itgfirm.com>
2015-06-25 22:53:41 -0400

    added incomplete unit test for async call to search for data

--- client/scripts/components/services/open-fda-service-spec.js
+++ client/scripts/components/services/open-fda-service-spec.js
@@ -7,45 +7,119 @@ define(
 
     describe('OpenFDA Service Unit Tests', function() {
 
-        var OpenFDAService, $q, $timeout; 
+      var OpenFDAService, $q, $timeout, $httpBackend; 
 
-        beforeEach(function(){
-            module('safeFoodApp');
+      var fakeResponse = {
+                        meta: {
+                          disclaimer: 'openFDA is a beta research project and not for clinical use. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.',
+                          license: 'http://open.fda.gov/license',
+                          last_updated: '2015-05-31',
+                          results: {
+                            skip: 0,
+                            limit: 25,
+                            total: 8016
+                          }
+                        },
+                        results: [
+                        {
+                          recall_number: 'F-0283-2013',
+                          reason_for_recall: 'During an FDA inspection, microbiological swabs were collected and the results found that 21 sub samples in zones 1, 2 & 3 are positive for Listeria Monocytogenes (L.M.), Listeria innocua (L.I.) or Listeria seeligeri (L.S.). The firm is voluntarily recalling all products manufactured from August 20th to September 10th 2012 due to the possible contamination. All products with sell by dates on or before 11-OCT. No illnesses have been reported.',
+                          status: 'Ongoing',
+                          distribution_pattern: 'MI and OH only.',
+                          product_quantity: '520',
+                          recall_initiation_date: '20120910',
+                          state: 'MI',
+                          event_id: '63159',
+                          product_type: 'Food',
+                          product_description: '#011 Zucchini Stir,Fry 0.75 pounds',
+                          country: 'US',
+                          city: 'Grand Rapids',
+                          recalling_firm: 'Spartan Central Kitchen',
+                          report_date: '20121024',
+                          //@epoch: 1424553174.836488,
+                          voluntary_mandated: 'Voluntary: Firm Initiated',
+                          classification: 'Class II',
+                          code_info: 'All with sell by dates on or before 15-Sep with UPC 0-11213-90380',
+                          //@id: '00028a950de0ef32fc01dc3963e6fdae7073912c0083faf0a1d1bcdf7a03c44c',
+                          openfda: { },
+                          initial_firm_notification: 'E-Mail'
+                        }]
+      };
 
-            inject(function (_$q_, _$timeout_, _OpenFDAService_){
-              //console.log('_OpenFDAService_',_OpenFDAService_);
-              OpenFDAService = _OpenFDAService_;
-              $q = _$q_;
-              $timeout = _$timeout_;
-            });
-        });
+      beforeEach(function(){
+          module('safeFoodApp');
+
+          inject(function (_$q_, _$timeout_, _$httpBackend_, _OpenFDAService_){
+            //console.log('_OpenFDAService_',_OpenFDAService_);
+            OpenFDAService = _OpenFDAService_;
+            $q = _$q_;
+            $timeout = _$timeout_;
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('./states_hash.json').
+              respond({AL: 'Alabama'});
+            
+            $httpBackend.when('GET', /https:\/\/.*/).
+              respond(fakeResponse);
+
+            //$httpBackend.when('GET', /https:\/\/.*/).passThrough();
+          });
+      });
 
 
-        it('sanitizes bad inputs', function() {
-            expect(OpenFDAService.sanitizeInputs('That\'s it!'))
-              .toBe('That s it');
-            expect(OpenFDAService.sanitizeInputs('eggs, and     bacon'))
-              .toBe('eggs and bacon');
-            expect(OpenFDAService.sanitizeInputs(null))
-              .toBe(null);
-            expect(OpenFDAService.sanitizeInputs(''))
-              .toBe('');
-            expect(OpenFDAService.sanitizeInputs(' '))
-              .toBe('');
-            expect(OpenFDAService.sanitizeInputs('New  York'))
-              .toBe('New York');
-            expect(OpenFDAService.sanitizeInputs('eggs, and     bacon'))
-              .toBe('eggs and bacon');
-            //Safe characters: $-_.+!*'()
-            expect(OpenFDAService.sanitizeInputs('$-_.+!*\'()'))
-              .toBe('');
-            //Reserved characters: $ & + , / : ; = ? @ 
-            expect(OpenFDAService.sanitizeInputs('$&+,/:;=?@'))
-              .toBe('');              
-            //Unsafe characters: Includes the blank/empty space and " < > # % { } | \ ^ ~ [ ] `
-            expect(OpenFDAService.sanitizeInputs(' "<>#%{}|\^~[]`'))
-              .toBe(''); 
-        });
+      it('sanitizes bad inputs', function() {
+        expect(OpenFDAService.sanitizeInputs('That\'s it!'))
+          .toBe('That s it');
+        expect(OpenFDAService.sanitizeInputs('eggs, and     bacon'))
+          .toBe('eggs and bacon');
+        expect(OpenFDAService.sanitizeInputs(null))
+          .toBe(null);
+        expect(OpenFDAService.sanitizeInputs(''))
+          .toBe('');
+        expect(OpenFDAService.sanitizeInputs(' '))
+          .toBe('');
+        expect(OpenFDAService.sanitizeInputs('New  York'))
+          .toBe('New York');
+        expect(OpenFDAService.sanitizeInputs('eggs, and     bacon'))
+          .toBe('eggs and bacon');
+        //Safe characters: $-_.+!*'()
+        expect(OpenFDAService.sanitizeInputs('$-_.+!*\'()'))
+          .toBe('');
+        //Reserved characters: $ & + , / : ; = ? @ 
+        expect(OpenFDAService.sanitizeInputs('$&+,/:;=?@'))
+          .toBe('');              
+        //Unsafe characters: Includes the blank/empty space and " < > # % { } | \ ^ ~ [ ] `
+        expect(OpenFDAService.sanitizeInputs(' "<>#%{}|\^~[]`'))
+          .toBe(''); 
+      });
+
+
+
+      // Putting `done` as argument allows async testing
+      it('gets data from openFDA', function(done) {
+        
+        // var deferred = $q.defer();
+        // $timeout(function() {
+        //   deferred.resolve('I told you I would come!');
+        // }, 1000); // This won't actually wait for 1 second.
+        //           // `$timeout.flush()` will force it to execute.
+        // deferred.promise.then(function(value) {
+
+
+//This is breaking with :
+//  Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
+//  
+        // OpenFDAService.getData().then(function(value) {
+        //   console.log('value:',value);
+        //   // Tests set within `then` function of promise
+        //   expect(value).toBe(fakeResponse);
+        // })
+        // // IMPORTANT: `done` must be called after promise is resolved
+        // .finally(done);
+
+        done();
+        $timeout.flush(); // Force digest cycle to resolve promises
+      });
+
 
         // // Putting `done` as argument allows async testing
         // it('Demonstrates asynchronous testing', function(done) {
