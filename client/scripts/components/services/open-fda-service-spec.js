54fdd123d28e02b0658057374bcb6a55195b305c
Phil Armour <phillip.armour@itgfirm.com>
2015-06-25 23:19:08 -0400

    fixed the non-working unit test for the call to get the openFDA search response

--- client/scripts/components/services/open-fda-service-spec.js
+++ client/scripts/components/services/open-fda-service-spec.js
@@ -9,42 +9,56 @@ define(
 
       var OpenFDAService, $q, $timeout, $httpBackend; 
 
+      //These should probably be loaded from JSON files in a directory for "mocks"
       var fakeResponse = {
-                        meta: {
-                          disclaimer: 'openFDA is a beta research project and not for clinical use. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.',
-                          license: 'http://open.fda.gov/license',
-                          last_updated: '2015-05-31',
-                          results: {
-                            skip: 0,
-                            limit: 25,
-                            total: 8016
-                          }
-                        },
-                        results: [
-                        {
-                          recall_number: 'F-0283-2013',
-                          reason_for_recall: 'During an FDA inspection, microbiological swabs were collected and the results found that 21 sub samples in zones 1, 2 & 3 are positive for Listeria Monocytogenes (L.M.), Listeria innocua (L.I.) or Listeria seeligeri (L.S.). The firm is voluntarily recalling all products manufactured from August 20th to September 10th 2012 due to the possible contamination. All products with sell by dates on or before 11-OCT. No illnesses have been reported.',
-                          status: 'Ongoing',
-                          distribution_pattern: 'MI and OH only.',
-                          product_quantity: '520',
-                          recall_initiation_date: '20120910',
-                          state: 'MI',
-                          event_id: '63159',
-                          product_type: 'Food',
-                          product_description: '#011 Zucchini Stir,Fry 0.75 pounds',
-                          country: 'US',
-                          city: 'Grand Rapids',
-                          recalling_firm: 'Spartan Central Kitchen',
-                          report_date: '20121024',
-                          //@epoch: 1424553174.836488,
-                          voluntary_mandated: 'Voluntary: Firm Initiated',
-                          classification: 'Class II',
-                          code_info: 'All with sell by dates on or before 15-Sep with UPC 0-11213-90380',
-                          //@id: '00028a950de0ef32fc01dc3963e6fdae7073912c0083faf0a1d1bcdf7a03c44c',
-                          openfda: { },
-                          initial_firm_notification: 'E-Mail'
-                        }]
+          meta: {
+            disclaimer: 'openFDA is a beta research project and not for '+
+              'clinical use. While we make every effort to ensure that data' +
+              ' is accurate, you should assume all results are unvalidated.',
+            license: 'http://open.fda.gov/license',
+            last_updated: '2015-05-31',
+            results: {
+              skip: 0,
+              limit: 25,
+              total: 8016
+            }
+          },
+          results: [
+          {
+            recall_number: 'F-0283-2013',
+            reason_for_recall: 'During an FDA inspection, microbiological'+
+              ' swabs were collected and the results found that 21 sub'+
+              ' samples in zones 1, 2 & 3 are positive for Listeria'+
+              ' Monocytogenes (L.M.), Listeria innocua (L.I.) or Listeria'+
+              ' seeligeri (L.S.). The firm is voluntarily recalling all '+
+              'products manufactured from August 20th to September 10th 2012 '+
+              'due to the possible contamination. All products with sell by'+
+              ' dates on or before 11-OCT. No illnesses have been reported.',
+            status: 'Ongoing',
+            distribution_pattern: 'MI and OH only.',
+            product_quantity: '520',
+            recall_initiation_date: '20120910',
+            state: 'MI',
+            event_id: '63159',
+            product_type: 'Food',
+            product_description: '#011 Zucchini Stir,Fry 0.75 pounds',
+            country: 'US',
+            city: 'Grand Rapids',
+            recalling_firm: 'Spartan Central Kitchen',
+            report_date: '20121024',
+            //@epoch: 1424553174.836488,
+            voluntary_mandated: 'Voluntary: Firm Initiated',
+            classification: 'Class II',
+            code_info: 'All with sell by dates on or before 15-Sep with UPC 0-11213-90380',
+            //@id: '00028a950de0ef32fc01dc3963e6fdae7073912c0083faf0a1d1bcdf7a03c44c',
+            openfda: { },
+            initial_firm_notification: 'E-Mail'
+          }]
       };
+
+      var fakeResponseProcessed = angular.copy(fakeResponse);
+      fakeResponseProcessed.results[0].recall_initiation_date = '09/10/2012';
+      fakeResponseProcessed.results[0].report_date = '10/24/2012';
 
       beforeEach(function(){
           module('safeFoodApp');
@@ -93,57 +107,18 @@ define(
       });
 
 
-
       // Putting `done` as argument allows async testing
       it('gets data from openFDA', function(done) {
         
-        // var deferred = $q.defer();
-        // $timeout(function() {
-        //   deferred.resolve('I told you I would come!');
-        // }, 1000); // This won't actually wait for 1 second.
-        //           // `$timeout.flush()` will force it to execute.
-        // deferred.promise.then(function(value) {
+        OpenFDAService.getData().then(function(value) {
+          // Tests set within `then` function of promise
+          expect(value).toEqual(fakeResponseProcessed);
+        })
+        // IMPORTANT: `done` must be called after promise is resolved
+        .finally(done);
 
-
-//This is breaking with :
-//  Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
-//  
-        // OpenFDAService.getData().then(function(value) {
-        //   console.log('value:',value);
-        //   // Tests set within `then` function of promise
-        //   expect(value).toBe(fakeResponse);
-        // })
-        // // IMPORTANT: `done` must be called after promise is resolved
-        // .finally(done);
-
-        done();
-        $timeout.flush(); // Force digest cycle to resolve promises
+        $httpBackend.flush(); // Force digest cycle to resolve promises
       });
 
-
-        // // Putting `done` as argument allows async testing
-        // it('Demonstrates asynchronous testing', function(done) {
-        //     //var deferred = $q.defer();
-        //     OpenFDAService.configure();
-
-        //     OpenFDAService.getData();
-
-        //     // $timeout(function() {
-        //     //     deferred.resolve('I told you I would come!');
-        //     // }, 1000); // This won't actually wait for 1 second.
-        //     //           // `$timeout.flush()` will force it to execute.
-
-        //     // deferred.promise
-
-        //     OpenFDAService.getData().then(function(value) {
-        //         console.log('value:' , value);
-        //         // Tests set within `then` function of promise
-        //         expect(value.results.leght>0).toBeTruthy();
-        //     })
-        //     // IMPORTANT: `done` must be called after promise is resolved
-        //     .finally(done);
-
-        //     $timeout.flush(); // Force digest cycle to resolve promises
-        // });
     });
 });
